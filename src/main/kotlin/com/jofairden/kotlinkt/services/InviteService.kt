dac9199c223fc763f6455eeffcb582d60dfa9ced
Jofairden <jofairden@gmail.com>
2020-01-07 21:31:39 +0100

    Retrofit InviteService

---
+++ src/main/kotlin/com/jofairden/kotlinkt/services/InviteService.kt
@@ -0,0 +1,18 @@
+package com.jofairden.kotlinkt.services
+
+import com.fasterxml.jackson.databind.JsonNode
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface InviteService {
+    @GET("invites/{invite.code}")
+    suspend fun getInvite(
+        @Path("invite.code") guildId: Int
+    ): JsonNode // Invite
+
+    @DELETE("invites/{invite.code}")
+    suspend fun deleteInvite(
+        @Path("invite.code") guildId: Int
+    ): JsonNode
+}
