a02ca2fbdbb8e2f6b65986713bcad9b67a978703
Jofairden <jofairden@gmail.com>
2020-01-07 21:47:50 +0100

    Retrofit EmojiService

--- src/main/kotlin/com/jofairden/kotlinkt/util/ServiceUtil.kt
+++ src/main/kotlin/com/jofairden/kotlinkt/util/ServiceUtil.kt
@@ -2,6 +2,7 @@ package com.jofairden.kotlinkt.util
 
 import com.jofairden.kotlinkt.services.AuditLogService
 import com.jofairden.kotlinkt.services.ChannelService
+import com.jofairden.kotlinkt.services.EmojiService
 import com.jofairden.kotlinkt.services.GatewayService
 import com.jofairden.kotlinkt.services.InviteService
 import com.jofairden.kotlinkt.services.UserService
@@ -9,8 +10,11 @@ import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
 
 internal object ServiceUtil {
+
+    private const val API_URL = "https://discordapp.com/api/"
+
     private val discordApi = Retrofit.Builder()
-        .baseUrl("https://discordapp.com/api/")
+        .baseUrl(API_URL)
         .addConverterFactory(JacksonConverterFactory.create())
         .build()
 
@@ -19,4 +23,5 @@ internal object ServiceUtil {
     val userService = discordApi.create(UserService::class.java)
     val auditLogService = discordApi.create(AuditLogService::class.java)
     val inviteService = discordApi.create(InviteService::class.java)
+    val emojiService = discordApi.create(EmojiService::class.java)
 }
