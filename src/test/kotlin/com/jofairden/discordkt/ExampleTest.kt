ef6c37abcfb487422906ee187839ca2bab2498b9
Jofairden <danielzondervan.nl@gmail.com>
2020-06-30 15:59:01 +0200

    Merge inspections workflows

--- src/test/kotlin/com/jofairden/discordkt/ExampleTest.kt
+++ src/test/kotlin/com/jofairden/discordkt/ExampleTest.kt
@@ -7,6 +7,6 @@ class ExampleTest {
     @Test
     fun `always succeeds`() {
         println("This test should always succeed and provide a baseline.")
-        assert(true);
+        assert(true)
     }
 }
