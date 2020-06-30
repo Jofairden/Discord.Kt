14b00fd12449ca4759c5c94936f5b15bbd636aa5
Jofairden <danielzondervan.nl@gmail.com>
2020-06-30 16:06:55 +0200

    Test run of workflow

--- src/test/kotlin/com/jofairden/discordkt/ExampleTest.kt
+++ src/test/kotlin/com/jofairden/discordkt/ExampleTest.kt
@@ -6,6 +6,7 @@ class ExampleTest {
 
     @Test
     fun `always succeeds`() {
+        // Testing run
         println("This test should always succeed and provide a baseline.")
         assert(true)
     }
